00ae1d39917bfa6e2b524f221163cf97a40719d6
calmesam01 <sanyamsareen01@gmail.com>
2017-11-14 02:46:24 +0530

    For Obama

---
+++ obama.R
@@ -0,0 +1,36 @@
+#Install the following packages in RStudio.
+install.packages("Rfacebook")
+install.packages("tm")
+install.packages("RCurl")
+install.packages("wordcloud")
+
+#Load these packages.
+library(Rfacebook)
+library(RCurl)
+library(tm)
+library(wordcloud)
+
+#Fill the token here('XXXX') that you get from https://developers.facebook.com/tools/explorer.
+token = 'XXXX'
+
+#Now, save 100 posts from Barack Obama's page into obama_page variable.
+obama_page = getPage("barackobama",token,n = 100)
+
+#Get the the first post.
+obama_post = getPost(post = obama_page$id[1],n = 100, token)
+
+#Get the replies to the comments.
+obama_message = (obama_post$comments)$message
+
+#Create the corpous after passing docs into VectorSource().
+docs = Corpus(VectorSource(obama_message))
+
+#Do the preproccesing with tm package.
+docs = tm_map(docs, removePunctuation)
+docs = tm_map(docs, removeNumbers)
+docs = tm_map(docs, tolower)
+docs = tm_map(docs, stripWhitespace)
+docs = tm_map(docs, removeWords,stopwords("en"))
+
+#At last, with wordcloud function, create one.
+wordcloud(docs, max.words = 100, random.order = FALSE)
